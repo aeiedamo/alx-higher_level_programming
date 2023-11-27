4391f1e2bf22fe15c2dc3a0855392fdfab3b921e
AlaaEldin <alaa12122000@gmail.com>
2023-11-27 22:24:08 +0000

    add #11

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,18 +2,18 @@
 
 int check_cycle(listint_t *list)
 {
-        listint_t *chk;
+	listint_t *chk;
 
-        if (!list || !list->next)
-                return (0);
+	if (!list || !list->next)
+		return (0);
 
-        for (chk = list->next; 
-                list && chk->next && chk->next->next; list = list->next,
-                chk = chk->next->next)
-        {
-                if (list == chk)
-                        return (1);
-        }
+	for (chk = list->next; 
+		list && chk->next && chk->next->next; list = list->next,
+		chk = chk->next->next)
+	{
+		if (list == chk)
+			return (1);
+	}
 
-        return (0);
+	return (0);
 }
