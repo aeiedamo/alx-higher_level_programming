27a494b06a29f18522cd0e766f9b8b5eadf668b6
AlaaEldin Mohamed <aeiedamo@gmail.com>
2023-11-30 21:44:50 +0300

    Fix #3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import sys
     num = 0
     for i in sys.argv:
-        if arg != sys.argv[0]:
-            num += int(arg)
+        if i != sys.argv[0]:
+            num += int(i)
     print(num)
