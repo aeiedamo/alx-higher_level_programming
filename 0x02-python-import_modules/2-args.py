256371bbee545257f0c897265bb6f182b1628cbd
AlaaEldin Mohamed <aeiedamo@gmail.com>
2023-11-30 21:37:40 +0300

    Fix 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,8 @@ if __name__ == "__main__":
     i = len(sys.argv) - 1
     print("{} arguments:".format(i))
     if i >= 1:
+        i = 0
         for arg in sys.argv:
             if i != 0:
                 print("{}: {}".format(i, arg))
-            i++
+            i += 1
