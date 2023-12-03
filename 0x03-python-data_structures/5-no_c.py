cb8ae71677e3e747f3fb3b0725aa38fd6e95a723
AlaaEldin Mohamed <aeiedamo@gmail.com>
2023-12-03 14:12:47 +0300

    Testing #5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
     if my_string:
-        my_string.replace('C', '')
-        my_string.replace('c', '')
+        my_string = my_string.replace('C', '')
+        my_string = my_string.replace('c', '')
     return (my_string)
