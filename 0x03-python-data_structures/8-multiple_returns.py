57901c9c6c3f324535a91918dc494563895e9427
AlaaEldin Mohamed <aeiedamo@gmail.com>
2023-12-04 22:48:10 +0300

    Fix #8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,4 +3,6 @@ def multiple_returns(sentence):
     return_tuple = ()
     if sentence:
         return_tuple += (len(sentence), sentence[0])
+    else:
+        return_tuple += (0, None)
     return (return_tuple)
