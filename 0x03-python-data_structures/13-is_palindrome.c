51c9e1099836c0f2362e0b48be74fc82113c584d
AlaaEldin Mohamed <aeiedamo@gmail.com>
2023-12-05 00:37:40 +0300

    Fix #13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,21 +1,31 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <stdio.h>
+/**
+ * compare - function to compare nodes
+ * @head: pointer to head node
+ * @comp: pointer to compared node
+ * Return: 1 if all previous comparisons were success, 0 otherwise
+*/
+int compare(listint_t **head, listint_t *comp)
+{
+	if (!comp)
+		return (1);
+	if (compare(head, comp->next) && (*head)->n == comp->n)
+	{
+		(*head) = (*head)->next;
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - function to check if a list is palindrome
+ * @head: pointer to head node
+ * Return: 0 if list is not palindrome, 1 otherwise
+*/
 
 int is_palindrome(listint_t **head)
 {
-    listint_t *compare = *head;
-
-    if (!head || !*head || !compare)
-        return (1);
-    
-    do
-    {
-        if (((*head)->n != compare->n))
-            return (0);
-        *head = (*head)->next;
-    } while (compare->next);
-
-    return (1);
-    
+	if (!head || !(*head))
+		return (1);
+	return(compare(head, *head));
 }
