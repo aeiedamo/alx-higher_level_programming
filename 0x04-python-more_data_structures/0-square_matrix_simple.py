7621945dca4e7161cbc0208c9f2881100b2bbc00
AlaEldin <aeiedamo@gmail.com>
2023-12-07 00:21:51 +0300

    Fix #0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,19 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    squares = []
-    if matrix:
-        for i in matrix:
-            neoRow = []
-            for j in i:
-                neoRow.append(j ** 2)
-            squares.append(neoRow)
+    squares = [list(map(lambda i: i ** 2, j)) for j in matrix]
     return (squares)
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
