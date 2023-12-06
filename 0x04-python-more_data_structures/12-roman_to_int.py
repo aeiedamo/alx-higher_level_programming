2e309dbf5639bd683e8fecae806bb80860dcaaaf
AlaEldin <aeiedamo@gmail.com>
2023-12-07 01:09:26 +0300

    Fix #12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,8 @@
 def roman_to_int(roman_string):
     num = 0
     if roman_string and type(roman_string) == str:
-        romanDigits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        romanDigits = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                       'C': 100, 'D': 500, 'M': 1000}
         for romanNum in reversed(roman_string):
             HinduArabic = romanDigits[romanNum]
             if num < (HinduArabic * 5):
