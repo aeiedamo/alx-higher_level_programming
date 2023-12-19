e480246c1b2e7bd2343ab57df04e5fab76edef56
AlaEldin <aeiedamo@gmail.com>
2023-12-19 23:52:26 +0300

    small fix #1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""define a Square class"""
 class Square:
     """to define a square"""
     def __init__(self, size):
-        """init defines a size and set size"""
+        """init defines a size
+        Args:
+            size: length for sqaure
+        """
         self.__size = size
