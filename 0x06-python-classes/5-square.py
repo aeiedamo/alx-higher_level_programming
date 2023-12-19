d351cd0eb91cb3acef2cc5439e49f712191f34ee
AlaEldin <aeiedamo@gmail.com>
2023-12-20 00:29:34 +0300

    Add solution for #5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -53,3 +53,13 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
+    def my_print(self):
+        """
+        function that prints in stdout the square with the character #
+        """
+        if (self.__size == 0):
+            print()
+        else:
+            for i in range(self.__size):
+                print("#"*self.__size)
