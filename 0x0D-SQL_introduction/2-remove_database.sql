fc86e60f135290f35eaec1c8067b1c75e12ffcb0
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-02-14 21:09:05 +0300

    Add file #4 && #5

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
 -- a script that deletes the database hbtn_0c_0 in your MySQL server.
-Drop DATABASE;
+Drop hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- a script that creates a table called first_table in the current database in your MySQL server.
+CREATE TABLE IF NOT EXISTS hbtn_0c_0 (
+	id INT,
+	name VARCHAR(256),
+);
