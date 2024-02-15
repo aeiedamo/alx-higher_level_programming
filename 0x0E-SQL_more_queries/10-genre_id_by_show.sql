89a4379680687d56a5a3cc079de837fe9f7336a2
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-02-15 23:13:56 +0300

    add files up to #11

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,2 @@
+-- a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT tv_shows.title, tv_shows.genre_id FROM tv_shows INNER JOIN tv_shows_genres ON tv_show.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_shows_genres.genre_id ASC;
