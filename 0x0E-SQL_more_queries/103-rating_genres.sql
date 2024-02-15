051404846af021dfa849b3f72c9a64defa69805c
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-02-15 23:37:57 +0300

    fix file #103

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,4 +1,5 @@
-ELECT name, SUM(tv_show_ratings.rate) 'rating'
+--  a script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+SELECT name, SUM(tv_show_ratings.rate) 'rating'
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
