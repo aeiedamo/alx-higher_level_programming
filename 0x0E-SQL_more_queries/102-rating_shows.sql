8082275190370874f22174353110da66fc4bcecd
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-02-15 23:36:58 +0300

    fix file #102

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,4 +1,5 @@
-ELECT title, SUM(tv_show_ratings.rate) 'rating'
+-- a script that lists all shows from hbtn_0d_tvshows_rate by their rating.
+SELECT title, SUM(tv_show_ratings.rate) 'rating'
 FROM tv_shows
 LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY title
