d4616ee69cce00d134274576d1887801f251b810
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-02-15 22:51:07 +0300

    fixing some files

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- a script that creates the database hbtn_0d_2 and the user user_0d_2.
 create database if not exists hbtn_0d_2;
-create user if not exists user_0d_2@localhost identified by user_0d_2_pwd;
+create user if not exists user_0d_2@localhost identified by 'user_0d_2_pwd';
 grant select on hbtn_0d_2.* to user_0d_2@localhost;
