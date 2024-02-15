a78077a334be9db6020ee287a47d537e6a0b28c9
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-02-15 23:16:49 +0300

    testing file £2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- a script that creates the database hbtn_0d_2 and the user user_0d_2.
 CERATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CERATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+CERATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY user_0d_2_pwd;
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
