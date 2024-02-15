c6d047f174a115b542cd12c72e92ceea39a3af66
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-02-15 23:02:44 +0300

    add files up to #7

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- a script that creates the database hbtn_0d_2 and the user user_0d_2.
-create database if not exists hbtn_0d_2;
-create user if not exists user_0d_2@localhost identified by 'user_0d_2_pwd';
-grant select on hbtn_0d_2.* to user_0d_2@localhost;
+CERATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CERATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
