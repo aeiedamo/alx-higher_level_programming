a06ab6321894a492e02e433f9ab0882deb308463
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-03-17 16:58:47 +0100

    Another test for #15

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -6,7 +6,7 @@ import sys
 from relationship_state import Base, State
 from relationship_city import City
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
