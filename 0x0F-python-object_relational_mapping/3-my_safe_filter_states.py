88bc49e28fa572b66c85b8e0956f3444558007a5
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-03-17 09:10:06 +0100

    Add solutions for tasks #0-#4 to 0x0F Object mapping in python

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+a script that lists all states from the database hbtn_0e_0_usa.
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    """Here we connect to MySQL database to list al states"""
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306,
+    )
+    current = db.cursor()
+    userinput = sys.argv[4]
+    current.execute("SELECT * FROM states WHERE name LIKE BINARY %s", (userinput,))
+    rows = current.fetchall()
+    for row in rows:
+        print(row)
+    current.close()
+    db.close()
