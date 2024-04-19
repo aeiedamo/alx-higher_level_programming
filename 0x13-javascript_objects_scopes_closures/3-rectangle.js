d3ac83b2f64f81ce37f37e5a0e58d404bc16c807
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-04-19 17:25:05 +0100

    Add files up to #3 to 0x13

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor(w, h) {
+    if (w < 1 || h < 1) return;
+    this.width = w;
+    this.height = h;
+  }
+
+  print() {
+    const ch = "X";
+    for (let i = 0; i < this.height; i++) console.log(ch.repeat(this.width));
+  }
+}
+
+module.exports = Rectangle;
