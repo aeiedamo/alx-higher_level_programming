9e641211cbce174c63d350b3ca515f789e4dffe0
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-04-20 07:50:14 +0100

    0x13 - Fix layout of code

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
-const Rectangle = require("./4-rectangle");
+const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
-  constructor(size) {
+  constructor (size) {
     super(size, size);
   }
 
-  charPrint(c) {
-    if (!c) c = "X";
+  charPrint (c) {
+    if (!c) c = 'X';
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
     }
