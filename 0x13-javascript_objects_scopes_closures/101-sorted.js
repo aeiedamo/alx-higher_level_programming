706dc01c4c96b30ecbfa87a98db50b673bd31ed8
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-04-20 09:21:50 +0100

    0x13 - add files up to #12

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const { dict } = require("./101-data.js");
+
+const dictFromArr = Object.entries(dict);
+
+const newObj = {};
+
+dictFromArr.forEach((element) => {
+  newObj[element[1]]
+    ? newObj[element[1]].push(element[0])
+    : (newObj[element[1]] = [element[0]]);
+});
+
+console.log(newObj);
