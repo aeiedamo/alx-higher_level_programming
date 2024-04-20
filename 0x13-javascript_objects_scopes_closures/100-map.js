3c1769544a79ee2d1d9fc294ee7a06ccfc637aa6
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-04-20 09:22:36 +0100

    0x13 - fix layout

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
-const { list } = require("./100-data.js");
+const { list } = require('./100-data.js');
 console.log(list);
 console.log(list.map((e, i) => e * i));
--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const { dict } = require("./101-data.js");
+const { dict } = require('./101-data.js');
 
 const dictFromArr = Object.entries(dict);
 
