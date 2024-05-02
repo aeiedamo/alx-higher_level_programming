d34e21aa053fe2846ab3060710e26e3670b915f6
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-05-02 15:51:58 +0100

    0x11 - add directory and files up to #2

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""a Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response."""
+
+if __name__ == "__main__":
+    import sys
+    import urllib.request
+
+    with urllib.request.urlopen(sys.argv[1]) as res:
+        print(res.headers["X-Request-Id"])
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,3 @@
+## 0x11. Python - Network #1
+
+# checkout the [PDF](./Project_0x11.Python-Network#1_ALXAfricaIntranet.pdf) for details :)
