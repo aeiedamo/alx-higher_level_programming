1884348325a24f2e67bf7367b8b0d809c4e70824
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-05-22 16:14:01 +0100

    0x14 - add #2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// a script that display the status code of a GET request.
+
+const req = require('request');
+const url = process.argv[2];
+
+req.get(url, function (error, response) {
+  if (error) console.log(error);
+  else console.log('code: ' + response.statusCode);
+});
