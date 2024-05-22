6014fff1d5c333f019dae634f2f8916184c2e142
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-05-22 16:06:13 +0100

    0x14 - add #0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// to read from file
+
+const FS = require("fs");
+FS.readFile(process.argv[2], "utf8", function (err, data) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
+});
---
+++ 0x14-javascript-web_scraping/README.md
@@ -0,0 +1,3 @@
+## 0x14. JavaScript - Web scraping
+
+# check out the [PDF](./Project* 0x14. JavaScript - Web scraping * ALX Africa Intranet.pdf) for more details :)
